debbae95fbea13374342429588d74ecc9cab2a20
deekast <96024516+deekast@users.noreply.github.com>
2024-04-24 11:40:32 -0400

    lesson deliveriable 1

--- classes/bird.py
+++ classes/bird.py
@@ -1,10 +1,19 @@
 class Bird:
+    description = "its a bird"
+
+    all_birds = []
 
     def __init__(self, name):
         self.name = name
+        Bird.all_birds.append(self)
 
     def __repr__(self):
         return f"Bird(name={self.name})"
 
     def tweet(self):
         return f"{self.name} is posting all their tweets... I mean X's"
+    
+    @classmethod
+    def fly(cls):
+        for bird in cls.all_birds:
+            print(f"{bird.name} is taking flight")
--- classes/fish.py
+++ classes/fish.py
@@ -1,11 +1,18 @@
 class Fish:
 
+    all_fish = []
+
     def __init__(self, name, length_in_inches):
         self.name = name
         self.length_in_inches = length_in_inches
+        Fish.all_fish.append(self)
 
     def __repr__(self):
         return f"Fish(name={self.name}, length_in_inches={self.length_in_inches})"
 
     def make_bubbles(self):
         return "bloop bloop"
+
+    @classmethod 
+    def num_fish(cls):
+        return len(cls.all_fish)
